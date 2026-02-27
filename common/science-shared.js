d42a6b85a16445936eaba570ab6a98734855795f
myo-n778 <myo-n778@myo-n778noMacBook-Pro.local>
2026-02-27 20:15:12 +0900

    Update GAS endpoint URL

--- common/science-shared.js
+++ common/science-shared.js
@@ -29,7 +29,7 @@
   }
 
   window.ScienceShared = {
-    gasUrl: "https://script.google.com/macros/s/AKfycbwALdSBTQ5l5Idhz6O-m-5P8ZKxU1e8NMqAKkFH9E8YvHNToP9n3cu6x1N2CsQtWAx-SA/exec",
+    gasUrl: "https://script.google.com/macros/s/AKfycbz-rUP1QhQL2KIrPy2zhjSqhJhwUiz1LiouHAgsf8Rw0hZVTre4FXQEzSO0T7tsMDEe/exec",
     normalizeUserName: normalizeUserName,
     formatDateYmd: formatDateYmd,
     buildEraSessionStats: buildEraSessionStats
